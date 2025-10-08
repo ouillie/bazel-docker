8aa582b4936cca867739dbb8592a9b882166649a
Will Noble <author@will.party>
2025-10-08 14:51:15 -0700

    Fix Mac bug verifier on Linux

--- is-docker-for-mac-still-broken-somehow.sh
+++ is-docker-for-mac-still-broken-somehow.sh
@@ -14,21 +14,20 @@ tmpdir="$(mktemp -d)"
 status=0
 
 if docker run \
-  --user=0 \
-  --env=USER=0 \
   --volume="$cwd":"$cwd":ro \
-  --volume="$tmpdir":"$tmpdir" \
+  --volume="$tmpdir":/home/ubuntu/output \
   --workdir="$cwd" \
   gcr.io/bazel-public/bazel:latest \
-  --output_user_root="$tmpdir" \
+  --output_user_root=/home/ubuntu/output \
   build @grpc//:grpc
 then
-  # Hopefully this happens eventually.
+  # This occurs on Linux today.
+  # Hopefully it happens on Mac eventually.
   echo -e "\nHuh, I guess it's fixed!"
   status=1
 else
   echo -e "\nYes, it is still broken!"
 fi
 
-rm -r "$tmpdir"
+yes | rm -r "$tmpdir"
 exit $status
